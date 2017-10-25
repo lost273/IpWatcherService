0c14020888cc3995c037ea5629912f5c313b8017
lost273 <dnk273@gmail.com>
2017-10-25 13:38:45 +0300

    25-10-2017

---
+++ IpWatcherService/Installer1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace IpWatcherService {
+    [RunInstaller(true)]
+    public partial class Installer1 : Installer {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public Installer1 () {
+            InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "Service1";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
---
+++ IpWatcherService/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace IpWatcherService {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main () {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+
+
+            if (Environment.UserInteractive) {
+                #if DEBUG
+                (new Logger()).Start();
+                Thread.Sleep(Timeout.Infinite);
+                #else
+                MessageBox.Show("Приложение должно быть установлено в виде службы Windows и не может быть запущено интерактивно.");
+                #endif
+            }
+            else {
+                ServiceBase.Run(ServicesToRun);
+            }
+
+
+
+
+            
+        }
+    }
+}
